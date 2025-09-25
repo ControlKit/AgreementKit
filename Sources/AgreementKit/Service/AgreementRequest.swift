79c4712951474b9733fc7119c63e5df33f59bb2e
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2025-09-25 18:38:25 +0330

    Add app version to request

--- Sources/AgreementKit/Service/AgreementRequest.swift
+++ Sources/AgreementKit/Service/AgreementRequest.swift
@@ -8,7 +8,8 @@
 import Foundation
 public struct AgreementRequest {
     public var name: String
-    public var appId: String = Bundle.main.bundleIdentifier ?? String()
+    public var appId: String
+    public var applicationVersion: String = Bundle.main.releaseVersionNumber ?? String()
     public var route: String = "https://tauri.ir/api/terms-and-conditions"
     public var deviceUUID: String = UUID().uuidString
     public var sdkVersion: String = agreementKit_Version
@@ -16,7 +17,7 @@ public struct AgreementRequest {
     var headers: [String: String] {
         return ["x-app-id": appId,
                 "x-sdk-version": sdkVersion,
-                "x-version": "1",
+                "x-version": applicationVersion,
                 "x-device-uuid": deviceUUID]
     }
     var params: [String: String] {
