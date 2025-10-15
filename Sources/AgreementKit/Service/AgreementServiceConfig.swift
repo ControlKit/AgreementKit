9739e2aecfd86ab289f94f1841e580eee23c005b
Maziar Saadatfar <maziar.saadatfar@gmail.com>
2025-10-15 17:31:42 +0330

    Remove the version from config

--- Sources/AgreementKit/Service/AgreementServiceConfig.swift
+++ Sources/AgreementKit/Service/AgreementServiceConfig.swift
@@ -11,17 +11,14 @@ public struct AgreementServiceConfig {
     public init(style: AgreementViewStyle = .darkMode,
                 name: String,
                 appId: String,
-                language: CKLanguage,
-                version: String) {
+                language: CKLanguage) {
         self.viewConfig = AgreementViewStyle.getViewConfigWithStyle(style: style, lang: language)
         self.appId = appId
-        self.version = version
         self.language = language
         self.name = name
     }
     public var name: String
     public var appId: String
-    public var version: String
     public var language: CKLanguage
     public var sdkVersion: String = agreementKit_Version
     public var viewConfig: AgreementViewConfig
